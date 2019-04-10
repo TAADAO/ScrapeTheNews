af372af98c7391ebf351137b1e8b500790f9c983
Robert O <romohund@gmail.com>
2019-04-10 18:19:35 -0500

    Tweaking..forever tweaking

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
-// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var hbs = require("express-handlebars");
+var mongoose = require('mongoose');
 
+var routes = require("./routes/routes.js");
 
-// Require the routes and use them
-var routes = require("./routes/routes");
-
-// Initialize Express
 var app = express();
 
-// set up the HBS view engine
-app.engine("handlebars", hbs({defaultLayout: "main", extname: "handlebars", partialsDir: [__dirname + "/views/partials"]}));
+app.engine("handlebars", hbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Use morgan for debug logging
 app.use(logger("dev"));
 
-// set up body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+	extended: true
 }));
 
-// set the public static directory
 app.use(express.static("public"));
 
-// Import routes
 app.use("/", routes);
 
-// Launch App
 var port = process.env.PORT || 8000;
 
 app.listen(port, function()
